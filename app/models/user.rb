40dece24cc1e1694b001223bb2dd772daecb43d1
Justin Wagner <jwag789@gmail.com>
2014-09-23 17:09:07 -0400

    Add users controller and sign up view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	has_secure_password
+	validates :email, :username, presence: true
+    validates :email, :username, uniqueness: true
+    validates :password, :length => {minimum: 4}
 end
