d8e203a3b26349c9e75986fa8b45b493f7ac9f40
Justin Wagner <jwag789@gmail.com>
2014-09-24 14:39:25 -0400

    Create conversation and message models and migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	has_many :conversations
+	validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 	validates :email, :username, presence: true
     validates :email, :username, uniqueness: true
     validates :password, :length => {minimum: 4}
